dbe6f2ebff9465ddc3ff29aa1ec20f3ad7104415
AndradeLucasS <95777438+AndradeLucasS@users.noreply.github.com>
2022-01-01 18:29:16 -0300

    v1.2

--- scripts/main.js
+++ scripts/main.js
@@ -1,54 +1,56 @@
-function giveC2Fres(){
-    var C2F = document.getElementById("c2f");
-    var valueC2F = C2F.value;
-    document.getElementById("c2fres").innerHTML = valueC2F + "ºC in fahrenheit degree is " + parseFloat(valueC2F*9/5 + 32)+ "ºF";
-    console.log(valueC2F*9/5);
-    if (valueC2F.length === 0){
-        document.getElementById("c2fres").innerHTML = "Insert a Celsius value above to return a Fahrenheit value here";
-    }
-}
+let convTemp = {
+    c2f: document.getElementById("c2f"),
+    c2k: document.getElementById("c2k"),
+    f2c: document.getElementById("f2c"),
+    f2k: document.getElementById("f2k"),
+    k2c: document.getElementById("k2c"),
+    k2f: document.getElementById("k2f"),
+    c2fRes: document.getElementById("c2fres"),
+    c2kRes: document.getElementById("c2kres"),
+    f2cRes: document.getElementById("f2cres"),
+    f2kRes: document.getElementById("f2kres"),
+    k2cRes: document.getElementById("k2cres"),
+    k2fRes: document.getElementById("k2fres"),
 
-function giveC2Kres(){
-    var C2K = document.getElementById("c2k");
-    var valueC2K = parseFloat(C2K.value);
-    document.getElementById("c2kres").innerHTML = valueC2K + "ºC in Kelvin is " + (valueC2K + 273.15) + "K";
-    if (C2K.value.length === 0){
-        document.getElementById("c2kres").innerHTML = "Insert a Celsius value above to return a Kelvin value here";
-    }
-}
+    genC2F(){
+        this.c2fRes.innerHTML = this.c2f.value +  "ºC in fahrenheit degree is " + parseFloat(this.c2f.value*9/5 + 32)+ "ºF";
+        if (this.c2f.value.length === 0){
+            convTemp.c2fRes.innerHTML = "Insert a Celsius value above to return a Fahrenheit value here";
+        }
+    },
 
-function giveF2Cres(){
-    var F2C = document.getElementById("f2c");
-    var valueF2C = F2C.value;
-    document.getElementById("f2cres").innerHTML = valueF2C + "ºF in Celsius degree is " + (valueF2C - 32)*5/9 + "ºC";
-    if (valueF2C.length === 0){
-        document.getElementById("f2cres").innerHTML = "Insert a Fahrenheit value above to return a Celsius value here";
-    }
-}
+    genC2K(){
+        this.c2kRes.innerHTML = this.c2k.value + "ºC in Kelvin is " + (parseFloat(this.c2k.value) + 273.15) + "K";
+        if (this.c2k.value.length === 0){
+            convTemp.c2kRes.innerHTML = "Insert a Celsius value above to return a Kelvin value here";
+        }
+    },
 
-function giveF2Kres(){
-    var F2K = document.getElementById("f2k");
-    var valueF2K = parseFloat(F2K.value);
-    document.getElementById("f2kres").innerHTML = valueF2K + "ºF in Kelvin is " + parseFloat((valueF2K - 32)*5/9 + 273.15) + "K";
-    if (F2K.value.length === 0){
-        document.getElementById("f2kres").innerHTML = "Insert a Fahrenheit value above to return a Kelvin value here";
-    }
-}
+    genF2C(){
+        this.f2cRes.innerHTML = this.f2c.value + "ºF in Celsius degree is " + (this.f2c.value - 32)*5/9 + "ºC";
+        if (this.f2c.value.length === 0){
+            convTemp.f2cRes.innerHTML = "Insert a Fahrenheit value above to return a Celsius value here";
+        }
+    },
 
-function giveK2Cres(){
-    var K2C = document.getElementById("k2c");
-    var valueK2C = K2C.value;
-    document.getElementById("k2cres").innerHTML = valueK2C + "K in Celsius degree is " + (valueK2C - 273.15) + "ºC";
-    if (valueK2C.length === 0){
-        document.getElementById("k2cres").innerHTML = "Insert a Kelvin value to return a Celsius value here";
-    }
-}
+    genF2K(){
+        this.f2kRes.innerHTML = this.f2k.value + "ºF in Kelvin is " + parseFloat((this.f2k.value - 32)*5/9 + 273.15) + "K";
+        if (this.f2k.value.length === 0){
+            convTemp.f2kRes.innerHTML = "Insert a Fahrenheit value above to return a Kelvin value here";
+        }
+    },
 
-function giveK2Fres(){
-    var K2F = document.getElementById("k2f");
-    var valueK2F = K2F.value;
-    document.getElementById("k2fres").innerHTML = valueK2F + "K in Fahrenheit degree is " + (((valueK2F - 273.15)* 9/5)+ 32) + "ºF";
-    if (valueK2F.length === 0){
-        document.getElementById("k2fres").innerHTML = "Insert a Kelvin value to return a Fahrenheit value here";
+    genK2C(){
+        this.k2cRes.innerHTML = this.k2c.value + "K in Celsius degree is " + (this.k2c.value - 273.15) + "ºC";
+        if (this.k2c.value.length === 0){
+            convTemp.k2cRes.innerHTML = "Insert a Kelvin value above to return a Celsius value here";
+        }
+    },
+
+    genK2F(){
+        this.k2fRes.innerHTML = this.k2f.value + "K in Fahrenheit degree is " + (((this.k2f.value - 273.15)* 9/5)+ 32) + "ºF";
+        if (this.k2f.value.length === 0){
+            convTemp.k2fRes.innerHTML = "Insert a Kelvin value above to return a Fahrenheit value here";
+        }
     }
-}
+};
